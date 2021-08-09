924ae549065f6e8fdf9661c42f79b77a493b320e
dsimp <55302995+dsimp@users.noreply.github.com>
2021-08-09 15:15:26 -0500

    creating user route implementing user registration

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const connectDB = require("./config/db");
+
 const app = express();
 
 connectDB();
+
+app.use(express.json({ extended: false }));
 
 app.get("/", (req, res) => res.send("API Running"));
 
