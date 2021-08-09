c9c08d3020d429e04ad4d7a0bd77db4e1d619588
dsimp <55302995+dsimp@users.noreply.github.com>
2021-08-09 17:59:00 -0500

    user authentication

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const router = express.Router();
-const { check, validationResult } = require("express-validator/check");
+const { check, validationResult } = require("express-validator");
 const gravatar = require("gravatar");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const config = require("config");
+
 const User = require("../../models/User");
 
 router.post(
@@ -52,7 +55,21 @@ router.post(
 
       await user.save();
 
-      res.send("User registered");
+      const payload = {
+        user: {
+          id: user.id,
+        },
+      };
+
+      jwt.sign(
+        payload,
+        config.get("jwtSecret"),
+        { expiresIn: 360000 },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
     } catch (err) {
       console.error(err.message);
       res.status(500).send("Server error");
