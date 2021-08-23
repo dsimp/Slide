9d73bce7eb566a0bd727356144e2f2eb8c51e3d4
dsimp <55302995+dsimp@users.noreply.github.com>
2021-08-23 14:31:24 -0500

    profile update, add ,delete

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -85,12 +85,13 @@ router.post(
           { new: true }
         );
 
-        return res, json(profile);
+        return res.json(profile);
       }
 
       profile = new Profile(profileFields);
 
       await profile.save();
+      res.json(profile);
     } catch (err) {
       console.error(err.message);
       res.status(500).send("Server Error");
@@ -99,5 +100,45 @@ router.post(
     console.log(profileFields.social.twitter);
   }
 );
+
+router.get("/", async (req, res) => {
+  try {
+    const profiles = await Profile.find().populate("user", ["name", "avatar"]);
+    res.json(profiles);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server Error");
+  }
+});
+
+router.get("/user/:user_id", async (req, res) => {
+  try {
+    const profile = await Profile.findOne({
+      user: req.params.user_id,
+    }).populate("user", ["name", "avatar"]);
+
+    if (!profile) return res.status(400).json({ msg: "Profile not found" });
+
+    res.json(profile);
+  } catch (err) {
+    console.error(err.message);
+    if (err.kind == "ObjectId") {
+      return res.status(400).json({ msg: "Profile not found" });
+    }
+    res.status(500).send("Server Error");
+  }
+});
+
+router.delete("/", auth, async (req, res) => {
+  try {
+    await Profile.findOneAndRemove({ user: req.user.id });
+
+    await User.findOneAndRemove({ _id: req.user.id });
+    res.json({ msg: "User removed" });
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server Error");
+  }
+});
 
 module.exports = router;
